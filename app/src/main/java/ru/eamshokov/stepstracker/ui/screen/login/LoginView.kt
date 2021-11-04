0ee76eaf3c17e8ced9e612cbccf4b322deb119f9
Eduard Amshokov <amd@mstagency.ru>
2021-11-04 22:29:47 +0300

    Screen structure and navigation setup

--- app/src/main/java/ru/eamshokov/stepstracker/ui/screen/login/LoginView.kt
+++ app/src/main/java/ru/eamshokov/stepstracker/ui/screen/login/LoginView.kt
@@ -1,6 +1,14 @@
 package ru.eamshokov.stepstracker.ui.screen.login
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import java.lang.reflect.Modifier
 
 @Composable
-fun LoginView(){}
+fun LoginView(
+    navController: NavController,
+    modifier:Modifier = Modifier()
+){
+    Text("login")
+}
